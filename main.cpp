76d7fec350869ed5303a0ae49a5b4c26b5f35ae5
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2024-06-27 18:32:44 +0200

    include a blank window sdl

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
+#include <SDL.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("SDL2 Window",
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          800, 600,
+                                          SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    SDL_Event e;
+    bool quit = false;
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        SDL_SetRenderDrawColor(SDL_GetRenderer(window), 0xFF, 0x00, 0x00, 0xFF);
+        SDL_RenderClear(SDL_GetRenderer(window));
+        SDL_RenderPresent(SDL_GetRenderer(window));
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
